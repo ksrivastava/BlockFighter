6b912d73f95aa0bad82a700b8aeb91d5f1e03e16
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-10-31 23:22:52 -0400

    Changed file location

---
+++ Assets/Scripts/GravitySwitch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class GravitySwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Hammer") {
+			Flip();
+		}
+	}
+
+	void Flip() {
+		var g = Physics2D.gravity;
+		g.y *= -1;
+		Physics2D.gravity = g;
+
+//		var Players = GameObject.FindGameObjectsWithTag ("Player");
+//
+//		foreach(GameObject player in Players) {
+//			var playerPoint = player.transform.parent;
+//			playerPoint.transform.Rotate(new Vector3(0, 0, 180));
+////			var rot = playerPoint.transform.rotation;
+////
+////			playerPoint.transform.rotation = rot;	                                 
+//		}
+	}
+}
