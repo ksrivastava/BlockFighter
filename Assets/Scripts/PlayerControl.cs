454c9e4b54dde079f007fa9460dc98a96f0a73ba
Bryant Ku <ku.bryant@gmail.com>
2014-11-03 16:17:15 -0500

    Four Players

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,18 +8,19 @@ public class PlayerControl : MonoBehaviour
 	[HideInInspector]
 	public bool jump = false;				// Condition for whether the player should jump.
 
+	public int playerNum;
 	public bool pickedUpObject = false;
 	
-	public float moveForce = 100f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 700f;			// Amount of force added when the player jumps.
+	private float moveForce = 100f;			// Amount of force added to move the player left and right.
+	private float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+	private float jumpForce = 700f;			// Amount of force added when the player jumps.
 	
 	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	public bool grounded = false;			// Whether or not the player is grounded.
+	private bool grounded = false;			// Whether or not the player is grounded.
 	
 	public bool isSecondPlayer = false;
 
-	bool onPlayer = false;
+	private bool onPlayer = false;
 
 	void Awake()
 	{
@@ -33,25 +34,23 @@ public class PlayerControl : MonoBehaviour
 	
 	void Update()
 	{
-
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
 		Vector3 groundPos = groundCheck.position;
 		groundPos.x = groundCheck.collider2D.bounds.min.x;
-
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
 		bool onGroundLeft = Physics2D.Linecast(transform.position, groundPos, 1 << LayerMask.NameToLayer("Ground"));
-
 		groundPos.x = groundCheck.collider2D.bounds.max.x;
-
 		bool onGroundRight = Physics2D.Linecast(transform.position, groundPos, 1 << LayerMask.NameToLayer("Ground"));
 
-		string otherPlayerLayer = (isSecondPlayer) ? "PlayerOne" : "PlayerTwo";
-		onPlayer = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer(otherPlayerLayer));
-
+		onPlayer = false;
+		for (int player = 1; player <= 4 && !onPlayer; player++) {
+			if(player != playerNum) {
+				onPlayer = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Player" + player));
+			}
+		}
 		grounded = onGroundLeft || onGroundRight || onPlayer;
 
-		// If the jump button is pressed and the player is grounded then the player should 	.
-		string jumpInput = isSecondPlayer ? "Jump2" : "Jump";
-		if(Input.GetButtonDown(jumpInput) && grounded)
+		// If the jump button is pressed and the player is grounded then the player should.
+		if(Input.GetButtonDown("Jump" + playerNum) && grounded)
 			jump = true;
 	}
 	
@@ -59,7 +58,7 @@ public class PlayerControl : MonoBehaviour
 	void FixedUpdate ()
 	{
 		// Cache the horizontal input.
-		float h = isSecondPlayer? Input.GetAxis("Horizontal2") : Input.GetAxis("Horizontal");
+		float h = Input.GetAxis ("Horizontal" + playerNum);
 		
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * rigidbody2D.velocity.x < maxSpeed)
@@ -93,7 +92,6 @@ public class PlayerControl : MonoBehaviour
 		}
 	}
 	
-	
 	void Flip ()
 	{
 		// Switch the way the player is labelled as facing.
@@ -103,5 +101,9 @@ public class PlayerControl : MonoBehaviour
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+	}
+
+	public int GetPlayerNum() {
+		return playerNum;
 	}
 }
