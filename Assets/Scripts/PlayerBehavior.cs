bd7b167214fe17f1fd4eed72f3e9fabb15996215
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-11-03 17:03:03 -0500

    Fixed heatmap

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -35,7 +35,7 @@ public class PlayerBehavior : MonoBehaviour {
 		if (healthBar.Health <= 0) {
 			
 			//TELL THE PLAYER EVENT CHECKER THAT YOU HAVE JUST DIED
-			PlayerEvents.RecordDeath(this.transform.parent.gameObject);
+			PlayerEvents.RecordDeath(this.gameObject);
 
 			Destroy(this.transform.parent.gameObject);
 
