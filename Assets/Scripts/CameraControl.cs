72ea308fe7b499e45a4b24f8e0d5e9c100fbdb57
Steven Louie <slouie@umich.edu>
2014-11-09 13:43:59 -0500

    enemy event

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -110,4 +110,8 @@ public class CameraControl : MonoBehaviour {
 
 	}
 
+	public GameObject GetLevelBounds() {
+		return levelBounds;
+	}
+
 }
---
+++ Assets/Scripts/Events/EnemyEvent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyEvent : MonoBehaviour, IEvent {
+
+	private float eventDuration = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Begin() {
+		EventController.DisplayMessage("ENEMIES!",2,new Vector2(0.5f,0.5f));
+		GameObject e1 = Instantiate (Resources.Load ("Events/Enemy")) as GameObject;
+		GameObject e2 = Instantiate (Resources.Load ("Events/Enemy")) as GameObject;
+
+		Vector3 pos = e1.transform.position;
+		pos.x = -20.0f;
+		pos.y = -10.0f;
+		e1.transform.position = pos;
+
+		pos.x = 20.0f;
+		e2.transform.position = pos;
+
+		Invoke ("End", eventDuration);	
+	}
+
+	public void End() {
+		EventController.EventEnd (EventType.EnemyEvent, EventType.PointLights, 3);
+	}
+
+	public void OnDestroy() {
+		End ();
+	}
+}
