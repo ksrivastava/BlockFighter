6fa2aea7d7de4d86cbf5a74c91a6bc14bbed08b2
Bryant Ku <ku.bryant@gmail.com>
2014-11-24 14:42:11 -0500

    map 2

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag == "Player") {
+			col.gameObject.GetComponent<PlayerBehavior> ().ReduceHealth (500);
+		} else if(col.gameObject.name.Contains("Rock")) {
+				Destroy(col.gameObject);
+		}
+
+	}
+}
