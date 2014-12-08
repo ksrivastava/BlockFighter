0108eb0cc1a19d7064061f5d711134a090e7921b
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-12-08 11:12:21 -0500

    Music

---
+++ Assets/Music/MusicPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	private int curAudio = 0;	
+	public AudioClip[] clips;
+	public AudioClip[] Clips {
+		get { return clips; }
+		set { 
+			clips = value;
+			curAudio = 0;
+			gameObject.audio.loop = false;
+		}
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		if (GameObject.FindGameObjectsWithTag ("MusicSource").Length > 1) {
+			Destroy(gameObject);
+		}
+		else {
+			gameObject.audio.enabled = true;
+		}
+	}
+
+	void Update() {	
+		if(!gameObject.audio.isPlaying) {
+			if(curAudio > clips.Length - 1) {
+				curAudio = clips.Length - 1;
+			}		
+			gameObject.audio.clip = clips[curAudio];
+			if (curAudio == clips.Length - 1) {
+				gameObject.audio.loop = true;
+			}
+			gameObject.audio.Play();
+			curAudio++;
+		}
+	}
+}
---
+++ Assets/MusicSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSetter : MonoBehaviour {
+
+	public AudioClip[] clips;
+
+	// Use this for initialization
+	void Start () {
+		GameObject source = GameObject.FindGameObjectWithTag ("MusicSource");
+		MusicPlayer player = source.GetComponent<MusicPlayer> ();
+		source.audio.Pause ();
+		player.Clips = clips;
+	}
+}
