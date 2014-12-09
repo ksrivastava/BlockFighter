e821e63f431bced5b2cb47fe8f99cff6fe4afc38
Anuraag Veturi <anuraagv@umich.edu>
2014-12-09 13:08:44 -0500

    trying a new map

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	Movement m;
+	public Vector2 displacement = new Vector2(10,0);
+	public float duration = 5;
+	public bool moveRightFirst = true;
+
+	// Use this for initialization
+	void Start () {
+		m = new Movement (this.gameObject);
+		if(moveRightFirst){
+			m.AddLine(this.transform.position,(Vector2)this.transform.position + displacement, duration);
+		} else {
+			m.AddLine(this.transform.position,(Vector2)this.transform.position - displacement, duration);
+		}
+
+		m.ChainLine (this.transform.position, duration);
+		m.SetRepeat ();
+		m.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m.Update ();
+		var pos = this.transform.position;
+		pos.z = -9;
+		this.transform.position = pos;
+	}
+}
