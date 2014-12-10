d24398bb20b1aa224446d3ab7b1925bc67747d0f
Bryant Ku <ku.bryant@gmail.com>
2014-12-10 14:56:33 -0500

    Map 4

---
+++ Assets/Scripts/Map Elements/TeleportWall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportWall : MonoBehaviour {
+	private float mapLength = 70f;
+	public bool leftWall;
+	void OnTriggerEnter2D(Collider2D other){
+		Vector2 pos = other.transform.position;
+		if (leftWall) {
+			pos.x += mapLength;
+		} else {
+			pos.x -= mapLength;
+		}
+		other.transform.position = pos;
+	}
+}
