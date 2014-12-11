2d4c75ff59cfc0cf3ef22208b6f16aa7529700a9
Bryant Ku <ku.bryant@gmail.com>
2014-12-11 15:28:45 -0500

    Bug with teleport wall

--- Assets/Scripts/Map Elements/TeleportWall.cs
+++ Assets/Scripts/Map Elements/TeleportWall.cs
@@ -5,12 +5,14 @@ public class TeleportWall : MonoBehaviour {
 	private float mapLength = 70f;
 	public bool leftWall;
 	void OnTriggerEnter2D(Collider2D other){
-		Vector2 pos = other.transform.position;
-		if (leftWall) {
-			pos.x += mapLength;
-		} else {
-			pos.x -= mapLength;
+		if(other.gameObject.layer != 8) {
+			Vector2 pos = other.transform.position;
+			if (leftWall) {
+				pos.x += mapLength;
+			} else {
+				pos.x -= mapLength;
+			}
+			other.transform.position = pos;
 		}
-		other.transform.position = pos;
 	}
 }
